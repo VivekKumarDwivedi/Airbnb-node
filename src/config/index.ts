a3cf8e36ce42e44f2d68e624c030cbfa09f05194
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2025-10-04 21:52:55 +0530

    Added sequelize config for migration

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,16 @@ import dotenv from 'dotenv';
 type ServerConfig={
 PORT:number
 };
- function loadEnv() {
+
+type DBConfig = {
+  DB_HOST: string,
+  DB_USER: string,
+  DB_PASSWORD: string,
+  DB_NAME: string
+}
+
+
+function loadEnv() {
   dotenv.config();
   console.log('Environment variables loaded successfully');
 }
@@ -12,4 +21,12 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 3000
+};
+
+export const dbConfig: DBConfig = {
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_USER: process.env.DB_USER ||  'root',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+  DB_NAME: process.env.DB_NAME || 'test_db',
+
 };
---
+++ src/config/sequelize.config.js
@@ -0,0 +1,4 @@
+require('ts-node/register');
+const config=require('./db.config');
+// console.log(config);
+module.exports=config;
