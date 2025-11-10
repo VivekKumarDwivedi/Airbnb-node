77089ad792044bbb18436201731000220a466744
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2025-11-10 23:19:38 +0530

    Added email sending through hbs and nodemailer

--- NotificationService/src/config/index.ts
+++ NotificationService/src/config/index.ts
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 type ServerConfig={
 PORT:number,
 REDIS_HOST?:string,
-REDIS_PORT?:number
+REDIS_PORT?:number,
+MAIL_PASS?:string,
+MAIL_USER?:string
 };
  function loadEnv() {
   dotenv.config();
@@ -15,5 +17,7 @@ loadEnv();
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 3000,
   REDIS_HOST: process.env.REDIS_HOST|| 'localhost',
-  REDIS_PORT: process.env.REDIS_PORT ?Number(process.env.REDIS_PORT) :6379
+  REDIS_PORT: process.env.REDIS_PORT ?Number(process.env.REDIS_PORT) :6379,
+  MAIL_PASS: process.env.MAIL_PASS || '',
+  MAIL_USER: process.env.MAIL_USER || ''
 };
--- NotificationService/src/processors/email.processor.ts
+++ NotificationService/src/processors/email.processor.ts
@@ -3,6 +3,9 @@ import { MAILER_QUEUE } from "../queues/mailer.queue";
 import  {NotificationDto } from "../dto/notification.dto";
 import { getRedisConnObject } from "../config/redis.config";
 import { MAILER_PAYLOAD } from "../producers/email.producer";
+import { renderMailTemplate } from "../templates/templates.handler";
+import { sendEmail } from "../services/mailer.service";
+import logger from "../config/logger.config";
 
 export const setupMailerWorker = () => {
         const emailProcessor = new Worker<NotificationDto>(
@@ -15,6 +18,10 @@ export const setupMailerWorker = () => {
            const payload = job.data;
            console.log(`Processing email for: ${JSON.stringify(payload)}`);
 
+            const emailContent = await renderMailTemplate(payload.templateId,payload.params);
+
+            await sendEmail(payload.to,payload.subject,emailContent);
+            logger.info(`Email sent to ${payload.to} with subject "${payload.subject}"`);
             
         },//process function
         {
