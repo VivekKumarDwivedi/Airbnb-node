faf2346f59112d55318eecb427a29a38dbfca94c
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2025-12-01 21:31:38 +0530

    Added db config

--- AuthInGo/main.go
+++ AuthInGo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"AuthInGo/app"
+	dbConfig "AuthInGo/config/db"
 	config "AuthInGo/config/env"
 )
 
@@ -9,6 +10,6 @@ func main() {
 	config.Load()
 	cfg := app.NewConfig()
 	app := app.NewApplication(cfg)
-
+	dbConfig.SetupDB()
 	app.Run()
 }
