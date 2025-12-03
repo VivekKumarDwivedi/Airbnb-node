6b0d5540e5022c9aaa13b903029d5cf721456af4
Vivek Kumar Dwivedi <vivekdwivedi498@gmail.com>
2025-12-03 23:41:22 +0530

    Added bcrypt

--- AuthInGo/services/user_service.go
+++ AuthInGo/services/user_service.go
@@ -2,11 +2,14 @@ package services
 
 import (
 	db "AuthInGo/db/repositories"
+	"AuthInGo/utils"
 	"fmt"
 )
 
 type UserService interface {
 	GetUserById() error
+	CreateUser() error
+	LoginUser() error
 }
 
 type UserServiceImpl struct {
@@ -21,7 +24,29 @@ func NewUserService(_userRepository db.UserRepository) UserService {
 
 func (u *UserServiceImpl) GetUserById() error {
 	fmt.Println("Fetching user in UserService")
-	u.userRepository.Create()
-	// u.userRepository.GetByID()
+	// u.userRepository.Create()
+	u.userRepository.GetByID()
 	return nil
+}
+
+func (u *UserServiceImpl) CreateUser() error {
+	fmt.Println("Creating user in UserService")
+	password := "example_password"
+	hashedPassword, err := utils.HashPassword(password)
+	if err != nil {
+		return err
+	}
+	u.userRepository.Create(
+		"username_example_2",
+		"user2@example.com",
+		hashedPassword,
+	)
+	return nil
+}
+
+func (u *UserServiceImpl) LoginUser() error {
+	response := utils.CheckPasswordHash("example_password", "")
+	fmt.Println("Login response:", response)
+	return nil
+
 }
